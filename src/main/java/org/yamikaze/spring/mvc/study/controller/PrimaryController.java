7393f8ab917f167cc775f9409188a3033bbf455c
yamikaze <1558642210@qq.com>
2017-12-22 00:25:58 +0800

    Spring最开始的Controller写法

---
+++ src/main/java/org/yamikaze/spring/mvc/study/controller/PrimaryController.java
@@ -0,0 +1,20 @@
+package org.yamikaze.spring.mvc.study.controller;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author qinluo
+ * @date 2017/12/22
+ */
+public class PrimaryController implements Controller{
+
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        System.out.println("Spring MVC最原始的写法!");
+        return new ModelAndView("basic/cf");
+    }
+}
